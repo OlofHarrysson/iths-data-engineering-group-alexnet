dd220cb2ee16f05668b2635e1a62ad67b45587b3
Hannes Hedberg <58343310+Hannesssss@users.noreply.github.com>
2023-08-23 11:22:50 +0200

    16 send summaries to discord (#25)
    * First attempt at Bot
    
    This bot includes a pip file, which may need to be replaced later.
    
    * Bot text formated
    
    Using markdown to update the ouput text and format it.
    
    * Reorganised Bot files
    
    * Create python_path.txt
    
    Attempt to fix MAKE
    
    * Update python_path.txt
    
    * Update .gitignore
    
    ---------
    
    Co-authored-by: William Lindstedt <w.lindstedt@gmail.com>

---
+++ Bot_testing/discord_bot_joke_request.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import requests
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def joke(ctx):
+    response = fetch_chuck_norris_joke()
+    await ctx.send(response)
+
+def fetch_chuck_norris_joke():
+    response = requests.get("https://api.chucknorris.io/jokes/random")
+    joke_data = response.json()
+    return joke_data.get("value", "No joke available")
+
+bot.run('YOUR_BOT_TOKEN_HERE')
