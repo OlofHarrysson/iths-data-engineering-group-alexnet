ecb4363eac8f0d363c0cb430dfea6299c2602dc5
Philip de Canésie <70288386+Canesp@users.noreply.github.com>
2023-08-23 13:48:18 +0200

    added pre-commit step to the action (#36)
    * added pre-commit step to the action
    
    * fixed some formating errors
    
    * fixed isort format on file

--- Bot_testing/discord_bot_chuckNorris.py
+++ Bot_testing/discord_bot_chuckNorris.py
@@ -1,23 +1,26 @@
-import requests
 import asyncio
 
+import requests
+
+
 async def send_discord_message(webhook_url, joke):
-    payload = {
-        "content": joke
-    }
+    payload = {"content": joke}
     response = requests.post(webhook_url, json=payload)
     response.raise_for_status()
+
 
 async def fetch_chuck_norris_joke():
     response = requests.get("https://api.chucknorris.io/jokes/random")
     joke_data = response.json()
     return joke_data.get("value", "No joke available")
 
+
 async def main():
     webhook_url = "https://discord.com/api/webhooks/1143558999237738577/ZJxXRlDKryW_BsLPHGfVa1JKeLAfj8A11yfpDSQbs4mMnc4Nb_AppoUxodbjHrA9Xke_"
-    
+
     joke = await fetch_chuck_norris_joke()
     await send_discord_message(webhook_url, joke)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
