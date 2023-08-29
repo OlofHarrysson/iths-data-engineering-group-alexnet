157b18c0720eb39c202c765e561b14d4b953a40e
Philip de Canésie <70288386+Canesp@users.noreply.github.com>
2023-08-29 13:43:54 +0200

    the function can now call summarize (#78)

--- src/newsfeed/get_summary.py
+++ src/newsfeed/get_summary.py
@@ -2,6 +2,22 @@ import json
 import os
 
 from newsfeed.latest_article import get_latest_article
+from newsfeed.summarize import create_summary_json
+
+
+def find_file(file_name, folder_path):
+    # list of files in folder.
+    file_list = os.listdir(folder_path)
+
+    # loops through the files.
+    for file in file_list:
+        # check if filename is in file.
+        if file_name in file:
+            # returns file path.
+            return os.path.join(folder_path, file)
+
+    # if no file was found returns none.
+    return None
 
 
 def get_summary(summary_type) -> str:
@@ -11,23 +27,26 @@ def get_summary(summary_type) -> str:
     # path to summarys.
     directory_path = f"data/data_warehouse/{summary_type}"
 
-    # list of files.
-    summary_list = os.listdir(directory_path)
+    # gets file path
+    file = find_file(article_id, directory_path)
 
-    # Iterates through the list of json files.
-    for summary in summary_list:
-        # checks if id in file name.
-        if article_id in summary:
-            # gets path to file.
-            file_path = os.path.join(directory_path, summary)
+    # checks so file is not None.
+    if file != None:
+        # reads file.
+        with open(file, "r") as file:
+            data = json.load(file)
 
-            # reads file.
-            with open(file_path, "r") as file:
-                data = json.load(file)
+            # returns summary.
+            return data["summary"], title, link
 
-                # returns summary.
-                return data["summary"], title, link
+    else:
+        # if file is none generate a summary.
+        print("found no file, creating one")
+        # path to article folder and file.
+        directory_path = "data/data_warehouse/mit/articles"
+        file = find_file(article_id, directory_path)
+        # creates a summary of article in type.
+        create_summary_json(file, "data/data_warehouse", "French")
 
-        else:
-            # if no summary was found.
-            return "found no summary."
+        # success message.
+        return f"Successfuly cretated a summary of type {summary_type}"
