983cc876f9ce1ceae532cb8667f92af932505812
William L <41084946+helveticae@users.noreply.github.com>
2023-08-30 22:51:36 +0200

    change dir

---
+++ app/src/Header.js
@@ -0,0 +1,18 @@
+// components/Header.js
+import React from 'react';
+import styles from './alx_header.module.css';
+
+const Header = () => {
+    return (
+      <header className={styles.alxHeader}>
+        <div className={styles.headerContent}>
+            <div className={styles.title}>
+              <h1>AAIN: Alexnet AI Newsfeed</h1>
+              <h2>Your Source for Intelligent News Analysis</h2>
+            </div>
+        </div>
+      </header>
+    );
+  };
+
+export default Header;
