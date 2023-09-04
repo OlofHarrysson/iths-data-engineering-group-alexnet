b14d37baedb6a1631a89df1402cdd76003a595ae
William L <41084946+helveticae@users.noreply.github.com>
2023-09-04 17:37:53 +0200

    wrap in actual function, add timer

--- app/src/generateJsonData.js
+++ app/src/generateJsonData.js
@@ -5,26 +5,35 @@
 const fs = require('fs');
 const path = require('path');
 
+function aggregateArticles(dirPath) {
+  const startTime = new Date();
+
+  const jsonData = [];
+
+  console.log(`Aggregating articles from ${dirPath}`);
+
+  fs.readdirSync(dirPath)
+    .map((file) => {
+      const filePath = path.join(dirPath, file);
+      const fileData = fs.readFileSync(filePath, 'utf-8');
+      const parsedData = JSON.parse(fileData);
+      return { filePath, parsedData };
+    })
+    .sort((a, b) => new Date(b.parsedData.published) - new Date(a.parsedData.published))
+    .slice(0, 15)
+    .forEach(({ parsedData }) => {
+      jsonData.push(parsedData);
+    });
+
+  const endTime = new Date();
+  const timeForFunctionToComplete = endTime - startTime;
+
+  const generatedContent = `// File generated with app/src/generateJsonData.js\nexport default ${JSON.stringify(jsonData, null, 2)};`;
+  fs.writeFileSync('aggregated_articles.js', generatedContent);
+
+  console.log(`Done in ${timeForFunctionToComplete} ms`);
+
+}
+
 const dirPath = '../../data/data_warehouse/mit/articles';
-const jsonData = [];
-
-console.log(`Aggregating articles from ${dirPath}...`);
-
-fs.readdirSync(dirPath)
-  .map((file) => {
-    const filePath = path.join(dirPath, file);
-    const fileData = fs.readFileSync(filePath, 'utf-8');
-    const parsedData = JSON.parse(fileData);
-    return { filePath, parsedData };
-  })
-  .sort((a, b) => new Date(b.parsedData.published) - new Date(a.parsedData.published)) // Sort by published date
-  .slice(0, 15) // Slice out 15 latest articles
-  .forEach(({ parsedData }) => {
-    jsonData.push(parsedData);
-  });
-
-// We could also put this in data_warehouse
-const generatedContent = `// File generated with app/src/generateJsonData.js\nexport default ${JSON.stringify(jsonData, null, 2)};`;
-fs.writeFileSync('aggregated_articles.js', generatedContent);
-
-console.log("Done!")
+aggregateArticles(dirPath);
