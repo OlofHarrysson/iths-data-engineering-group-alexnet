09bf064e3334c52450939424716223eec6b8eb4e
William L <41084946+helveticae@users.noreply.github.com>
2023-09-04 14:46:00 +0200

    update footer with placeholders

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import jsonData from './aggregated_articles';
 import './App.css';
 import Header from './Header';
-import ArticleCard from './articleCard'; // Import the new component
+import ArticleCard from './ArticleCard'; // Import the new component
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -15,16 +15,30 @@ function App() {
   return (
     <div className={darkMode ? 'dark-mode' : ''}>
       <Header />
-
-      <div className="dark-mode-toggle" onClick={toggleDarkMode}>
-        <span>Toggle Dark Mode</span>
-      </div>
-
       <div className="card-space">
         {jsonData.map((item, index) => (
           <ArticleCard key={index} item={item} darkMode={darkMode} />
         ))}
       </div>
+    <div className="bottom_footer">
+      <div className="dark-mode-toggle" onClick={toggleDarkMode}>
+        <span>Toggle Dark Mode</span>
+    </div>
+  <div className="example-text-column">
+    <div className="example-text"><p>Example column: 3 items</p></div>
+    <div className="example-text"><p>Example data: 20 ms</p></div>
+    <div className="example-text"><p>What stats we want here?</p></div>
+
+  </div>
+  <div className="example-text-column">
+    <div className="example-text"><p>Example column: 1 item</p></div>
+
+  </div>
+</div>
+
+
+
+
     </div>
   );
 }
