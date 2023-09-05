27e0bf24304066a4faf35e3a801928d4014b387b
William Lindstedt <w.lindstedt@gmail.com>
2023-09-05 10:15:08 +0200

    109 dashboard (#113)
    * reduce font size
    
    * color, spacing, padding adjustments
    
    * improved spacing
    
    * sort & slice 15 latest articles
    
    * improved link formatting
    
    * improved modularity
    
    * update footer with placeholders
    
    * move script folder, add console.logs
    
    * wrap in actual function, add timer
    
    * renames, fix new imports
    
    * updated comments regarding 'fs' (Important!)
    
    * updated comments regarding 'fs' (Important!)
    
    * cookies fix and improved comments
    
    * Revert "cookies fix and improved comments"
    
    This reverts commit 0c48ef9c5677493cfcea6d5b022f5268af03552b.
    
    * cookies fix and improved comments
    
    ---------
    
    Co-authored-by: William L <41084946+helveticae@users.noreply.github.com>

--- app/src/App.js
+++ app/src/App.js
@@ -1,36 +1,46 @@
 import React, { useState } from 'react';
 import jsonData from './aggregated_articles';
 import './App.css';
-import Header from './Header';
+import Header from './alxHeader';
+import ArticleCard from './articleCard';
+
+import { toggleDarkMode } from './darkMode';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
-  const toggleDarkMode = () => {
-    console.log('Toggle dark mode from App.js');
+  const handleToggleDarkMode = () => {
+    toggleDarkMode(); // Call toggleDarkMode function from darkMode.js
     setDarkMode(prevDarkMode => !prevDarkMode);
   };
 
   return (
     <div className={darkMode ? 'dark-mode' : ''}>
       <Header />
-
-      <div className="dark-mode-toggle" onClick={toggleDarkMode}>
-        <span>Toggle Dark Mode</span>
-      </div>
-
       <div className="card-space">
         {jsonData.map((item, index) => (
-          <div className={`articles-card ${darkMode ? 'dark-mode-card' : ''}`} key={index}>
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
-            <div className="article-footer">
-            <a href={item.link}>🌐 Full Article</a>
-            <p>{item.published}</p>
-            </div>
-          </div>
+          <ArticleCard key={index} item={item} darkMode={darkMode} />
         ))}
       </div>
+    <div className="bottom_footer">
+      <div className="dark-mode-toggle" onClick={handleToggleDarkMode}>
+        <span>Toggle Dark Mode</span>
+    </div>
+  <div className="example-text-column">
+    <div className="example-text"><p>Example column: 3 items</p></div>
+    <div className="example-text"><p>Example data: 20 ms</p></div>
+    <div className="example-text"><p>What stats we want here?</p></div>
+
+  </div>
+  <div className="example-text-column">
+    <div className="example-text"><p>Example column: 1 item</p></div>
+
+  </div>
+</div>
+
+
+
+
     </div>
   );
 }
