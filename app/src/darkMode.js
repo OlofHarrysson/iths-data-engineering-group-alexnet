098dd73389e7cb04d32a72fc080c7fcd18a675fc
William Lindstedt <w.lindstedt@gmail.com>
2023-09-05 10:15:08 +0200

    109 dashboard (#113)
    * reduce font size
    
    * color, spacing, padding adjustments
    
    * improved spacing
    
    * sort & slice 15 latest articles
    
    * improved link formatting
    
    * improved modularity
    
    * update footer with placeholders
    
    * move script folder, add console.logs
    
    * wrap in actual function, add timer
    
    * renames, fix new imports
    
    * updated comments regarding 'fs' (Important!)
    
    * updated comments regarding 'fs' (Important!)
    
    * cookies fix and improved comments
    
    * Revert "cookies fix and improved comments"
    
    This reverts commit 0c48ef9c5677493cfcea6d5b022f5268af03552b.
    
    * cookies fix and improved comments
    
    ---------
    
    Co-authored-by: William L <41084946+helveticae@users.noreply.github.com>

--- app/src/darkMode.js
+++ app/src/darkMode.js
@@ -1,44 +1,50 @@
 // darkMode.js
 
-// Function to set a cookie
-function setCookie(name, value, days) {
-    const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
-    document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
-  }
+// Set a cookie with a specified name, value, and optional expiration period in days
+export function setCookie(name, value, days) {
+  const expires = new Date(Date.now() + days * 86400000); // 86400000 milliseconds / day
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires.toUTCString()}; path=/`;
+}
 
-  // Function to get a cookie value
-  function getCookie(name) {
-    const cookies = document.cookie.split('; ');
-    for (const cookie of cookies) {
-      const [cookieName, cookieValue] = cookie.split('=');
-      if (cookieName === name) {
-        return decodeURIComponent(cookieValue);
-      }
+// Get a cookie value by name
+export function getCookie(name) {
+
+  // Splits the document's cookies into an array
+  const cookies = document.cookie.split('; ');
+
+  // Iterate through the array, very similar to Python
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split('=');
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
     }
-    return null;
   }
+  return null;
+}
 
-  // Function to toggle dark mode
-  function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
+// Toggle dark mode
+export function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
 
-    // Save the dark mode preference to a cookie
-    const darkModeEnabled = body.classList.contains('dark-mode');
-    setCookie('darkMode', darkModeEnabled ? '1' : '0', 30);
+  // Save dark mode preference to a cookie
+  const darkModeEnabled = body.classList.contains('dark-mode');
+  const darkModePref = darkModeEnabled ? '1' : '0';
+  setCookie('dark_mode', darkModePref, 365);
 
-    // Log the toggled message
-    console.log('Dark mode toggled');
+  console.log('Dark mode toggled');
+}
+
+// Initialize dark mode on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModePref = getCookie('dark_mode');
+  if (darkModePref === '1') {
+    document.body.classList.add('dark-mode');
   }
+});
 
-  // Toggle dark mode on button click
-  const toggleButton = document.getElementById('dark-mode-toggle');
+// Toggle dark mode on button click
+const toggleButton = document.getElementById('dark-mode-toggle');
+if (toggleButton) {
   toggleButton.addEventListener('click', toggleDarkMode);
-
-  // Check and apply dark mode preference on page load
-  document.addEventListener('DOMContentLoaded', () => {
-    const darkModePref = getCookie('darkMode');
-    if (darkModePref === '1') {
-      document.body.classList.add('dark-mode');
-    }
-  });
+}
