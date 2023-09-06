58aa70f80f1082944a23be7c96ff63ab5d78d0e9
Philip de Canésie <70288386+Canesp@users.noreply.github.com>
2023-09-06 12:49:49 +0200

    119 sql db (#122)
    * airflow credentials and db
    
    * Boilerplate
    
    * docker-compose
    
    * install requirements
    
    * new basic config workaround
    
    * added py file that insert data to db
    
    * made a function for storing data in db.
    
    * fixed db can now take in summaries
    
    * fixed blog -> blog_name
    
    * added create table function
    
    * added some comments
    
    * small fix
    
    * Update requirements.txt
    
    * Update Dockerfile
    
    * Update test_datatypes.py
    
    * Update test_extract_articles.py
    
    ---------
    
    Co-authored-by: William L <41084946+helveticae@users.noreply.github.com>

---
+++ src/newsfeed/data_parser.py
@@ -0,0 +1,86 @@
+import json
+import os
+from urllib.parse import unquote
+
+from sqlalchemy import create_engine, insert
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.automap import automap_base
+
+
+def get_data(path: str) -> list:
+    if os.path.isfile(path):
+        with open(path) as data:
+            return [json.load(data)]
+
+    if os.path.isdir(path):
+        data_list = []
+
+        files = os.listdir(path)
+
+        for file in files:
+            with open(os.path.join(path, file)) as data:
+                data_list.append(json.load(data))
+
+        return data_list
+
+    raise ValueError(f"Error: '{path}' is not a path to a folder or file.")
+
+
+def store_in_database(table_name: str, path: str = None, data: object = None):
+    data_list = []
+
+    if path is None and data is None:
+        raise ValueError(
+            "Error no data was provided, need to provide either 'path' or 'data' object or both."
+        )
+
+    if path != None:
+        data_list += get_data(path)
+        print(data_list)
+
+    if data != None:
+        data_list.append[data]
+
+    with open("api-key.json") as file:
+        data = json.load(file)
+
+        username = data["DB_username"]
+        password = data["DB_password"]
+
+    server_name = "localhost"
+    database_name = "postgres"
+
+    # postgreSQL database connection URL
+    DB_URL = f"postgresql://{username}:{password}@{server_name}/{database_name}"
+
+    print("Connecting to database using URL string:")
+
+    try:
+        engine = create_engine(DB_URL)
+        with engine.connect() as connection:
+            print(f"Successfully connected to {database_name}!")
+    except Exception as e:
+        print("Error while connecting to database:\n")
+        print(e)
+
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+
+    db_table = Base.classes.get(table_name)
+
+    for item in data_list:
+        query = insert(db_table).values(**item)
+
+        with engine.connect() as conn:
+            try:
+                result = conn.execute(query)
+                conn.commit()
+            except IntegrityError as e:
+                print(f"Error: {e}. Skipping duplicate row with ID {item['unique_id']}.")
+                conn.rollback()  # Rollback the transaction to keep the database in a consistent state
+
+
+store_in_database(
+    "blog_summaries",
+    path="data/data_warehouse/mit/summaries/french/a57cdb59-4cac-5ad7-ba41-f437d528ee28_french.json",
+)
