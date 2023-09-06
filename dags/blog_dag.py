6dfa436946b378575ae7ec77801a44092f1151fc
Rufshod <alexander@11a.se>
2023-09-06 13:20:05 +0200

    added the necessary PH functions

--- dags/blog_dag.py
+++ dags/blog_dag.py
@@ -18,6 +18,26 @@ def blog_scraper(**kwargs):
     pass
 
 
+def create_table(**kwargs):
+    pass
+
+
+def data_parser(**kwargs):
+    pass
+
+
+def summary_parser(**kwargs):
+    pass
+
+
+def discord(**kwargs):
+    pass
+
+
+def app(**kwargs):
+    pass
+
+
 ## ↑↑☻ PLACEHOLDER FUNCTIONS ↑↑☻ ##
 
 
@@ -54,6 +74,12 @@ start = EmptyOperator(
     dag=dag,
 )
 
+create_table = PythonOperator(
+    task_id="create_table",
+    python_callable=create_table,
+    dag=dag,
+)
+
 choose_branch_task = BranchPythonOperator(
     task_id="choose_branch",
     python_callable=choose_branch,
@@ -73,6 +99,31 @@ blog_scraper_task = PythonOperator(
     dag=dag,
 )
 
+data_parser_task = PythonOperator(
+    task_id="data_parser",
+    python_callable=data_parser,
+    dag=dag,
+)
+
+summary_parser_task = PythonOperator(
+    task_id="summary_parser",
+    python_callable=summary_parser,
+    dag=dag,
+)
+
+discord_task = PythonOperator(
+    task_id="discord",
+    python_callable=discord,
+    dag=dag,
+)
+
+app_task = PythonOperator(
+    task_id="app",
+    python_callable=app,
+    dag=dag,
+)
+
+
 join = EmptyOperator(
     task_id="join",
     dag=dag,
@@ -83,9 +134,11 @@ end = EmptyOperator(
     dag=dag,
 )
 
-start >> choose_branch_task
+start >> create_table >> choose_branch_task
 choose_branch_task >> download_blogs_from_rss_task
 choose_branch_task >> blog_scraper_task
 download_blogs_from_rss_task >> join
 blog_scraper_task >> join
-join >> end
+join >> data_parser_task >> summary_parser_task
+summary_parser_task >> discord_task >> end
+summary_parser_task >> app_task >> end
