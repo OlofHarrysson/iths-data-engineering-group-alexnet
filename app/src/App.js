9c91b475f67c135610a1c825723a68071fd81c9d
Hannesssss <hannes.hedberg@hotmail.se>
2023-09-07 21:38:24 +0200

    Added Live Search Feature for Real-time Filtering
    Implemented real-time search filtering in the app by utilizing React state updates and JavaScript's filter method, enabling users to instantly see filtered articles as they type in the search bar.

--- app/src/App.js
+++ app/src/App.js
@@ -1,52 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import jsonData from './aggregated_articles'; // Import jsonData
+import jsonData from './aggregated_articles';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-import ArticleCard from './articleCard'; // Import ArticleCard
-import { toggleDarkMode, getCookie, setCookie } from './darkMode'; // Import setCookie
+import ArticleCard from './articleCard';
+import { toggleDarkMode, getCookie, setCookie } from './darkMode';
 
 function App() {
-  // Retrieve the dark mode preference from the cookie
   const initialDarkModePref = getCookie('dark_mode');
-  const [darkMode, setDarkMode] = useState(initialDarkModePref === '1'); // Set initial state based on cookie
+  const [darkMode, setDarkMode] = useState(initialDarkModePref === '1');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredArticles, setFilteredArticles] = useState([]);
+
+  useEffect(() => {
+    const filtered = filterArticles(searchQuery);
+    setFilteredArticles(filtered);
+  }, [searchQuery]);
 
   const handleToggleDarkMode = () => {
-    toggleDarkMode(); // Call toggleDarkMode function from darkMode.js
+    toggleDarkMode();
     const newDarkModeValue = !darkMode;
     setDarkMode(newDarkModeValue);
-
-    // Update the dark mode preference in the cookie
     setCookie('dark_mode', newDarkModeValue ? '1' : '0', 365);
   };
 
-  useEffect(() => {
-    // This effect runs when the component mounts
-    // You can use it for other initializations if needed
-  }, []);
+  const filterArticles = (query) => {
+    if (!query) {
+      return jsonData;
+    }
+
+    return jsonData.filter((article) =>
+      article.title.toLowerCase().includes(query.toLowerCase())
+    );
+  };
 
   return (
     <div className={darkMode ? 'dark-mode' : ''}>
-      <Header />
-
-      {/* Add the Search bar */}
-      <div id="search-bar">
-        <form>
-          <input
-            type="text"
-            id="search-input"
-            placeholder="Search..."
-          />
-          <button type="submit">Search</button>
-        </form>
-      </div>
-
+      <Header setSearchQuery={setSearchQuery} />
       <div className="card-space">
-        {jsonData.map((item, index) => (
+        {filteredArticles.map((item, index) => (
           <ArticleCard key={index} item={item} darkMode={darkMode} />
         ))}
       </div>
-
       <Footer handleToggleDarkMode={handleToggleDarkMode} />
     </div>
   );
--- app/src/Header.js
+++ app/src/Header.js
@@ -1,18 +1,30 @@
-// components/Header.js
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = () => {
-    return (
-      <header className={styles.alxHeader}>
-        <div className={styles.headerContent}>
-            <div className={styles.title}>
-              <h1>AAIN: Alexnet AI Newsfeed</h1>
-              <h2>Your Source for Intelligent News Analysis</h2>
-            </div>
+const Header = ({ setSearchQuery }) => {
+  return (
+    <header className={styles.alxHeader}>
+      <div className={styles.headerContent}>
+        <div className={styles.title}>
+          <h1>AAIN: Alexnet AI Newsfeed</h1>
+          <h2>Your Source for Intelligent News Analysis</h2>
         </div>
-      </header>
-    );
-  };
+
+        {/* Add the Search bar */}
+        <div id="search-bar">
+          <form>
+            <input
+              type="text"
+              id="search-input"
+              placeholder="Search..."
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
