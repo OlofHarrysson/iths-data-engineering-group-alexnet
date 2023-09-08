950a8a91124e9aff52d6a4f1cb5c889a02ba7af2
Rufshod <alexander@11a.se>
2023-09-08 11:47:01 +0200

    Updated test_extract

--- tests/test_extract_articles.py
+++ tests/test_extract_articles.py
@@ -6,10 +6,20 @@ from bs4 import BeautifulSoup
 from newsfeed import extract_articles
 
 
+@pytest.fixture
+def mock_open(monkeypatch):
+    json_data = json.dumps({"DB_username": "username", "DB_password": "password"})
+
+    def mock_open_func(*args, **kwargs):
+        return [json_data]
+
+    monkeypatch.setattr("builtins.open", mock_open_func)
+
+
 @pytest.mark.parametrize(
     "input_str, expected_result", [("Sample Title", str), ("", None), (None, None)]
 )
-def test_create_uuid_from_string(input_str, expected_result):
+def test_create_uuid_from_string(input_str, expected_result, mock_open):
     result = extract_articles.create_uuid_from_string(input_str)
     if expected_result:
         assert result is not None
@@ -18,14 +28,14 @@ def test_create_uuid_from_string(input_str, expected_result):
         assert result is None
 
 
-def test_extract_mit_articles_from_xml():
+def test_extract_mit_articles_from_xml(mock_open):
     sample_xml = "<rss><channel></channel></rss>"
     parsed_xml = BeautifulSoup(sample_xml, "xml")
     articles = extract_articles.extract_mit_articles_from_xml(parsed_xml, "sample blog_name")
     assert articles == []
 
 
-def test_extract_tensorflow_articles_from_xml():
+def test_extract_tensorflow_articles_from_xml(mock_open):
     sample_xml = "<rss><channel></channel></rss>"
     parsed_xml = BeautifulSoup(sample_xml, "xml")
     articles = extract_articles.extract_tensorflow_articles_from_xml(parsed_xml, "sample blog_name")
